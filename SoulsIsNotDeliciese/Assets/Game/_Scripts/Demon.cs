7d2b2b59e398281cf47f4a9c87f36d7d9b623268
letopeace <dumanaskerbek@gmail.com>
2026-06-07 13:27:41 +0500

    Demon

--- SoulsIsNotDeliciese/Assets/Game/_Scripts/Demon.cs
+++ SoulsIsNotDeliciese/Assets/Game/_Scripts/Demon.cs
@@ -1,5 +1,8 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening.Plugins.Core.PathCore;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using TMPro;
 using UnityEngine;
@@ -71,6 +74,7 @@ public class Demon : MonoBehaviour
 
 			float waitTime = Mathf.Clamp(replices[i].Replices.Length, 0, 100) * 0.1f;
 			waitTime = WaitCurve.Evaluate(waitTime);
+			Debug.Log($"WaitTime = {waitTime * 10}");
 			await WaitReplic(waitTime*10);
 		}
 
@@ -91,13 +95,13 @@ public class Demon : MonoBehaviour
 		{
 			time += Time.deltaTime;
 
+			await UniTask.Yield();
+
 			if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.E) || isSkipping)
 			{
 				isSkipping = false;
 				return;
 			}
-
-			await UniTask.Yield();
 		}
 	}
 
@@ -113,8 +117,73 @@ public class Demon : MonoBehaviour
 	[Serializable]
 	public class Speach
 	{
-		public string Replices;
-		public string AnimPos;
-		public float animCrossFades;
+		public string Replices = "Default Replices";
+		public string AnimPos = "DemonDefaultPose";
+		public float animCrossFades = 0;
 	}
+
+	[ContextMenu("Parse")]
+	public void Parse()
+	{
+		var dialogues = Parse1();
+		Debug.Log($"Count dialogues List: {dialogues.Count}");
+
+		Replices[] replices = new Replices[dialogues.Count / 2];
+
+		for (int i = 0; i < dialogues.Count ; i += 2)
+		{
+			replices[i / 2] = new Replices();
+			replices[i / 2].DayStartReplices = new Speach[dialogues[i].Length];
+			replices[i / 2].FoodEndReplices = new Speach[dialogues[i + 1].Length];
+
+			for (int j = 0; j < dialogues[i].Length; j++)
+			{
+				string replice = dialogues[i][j];
+				replices[i / 2].DayStartReplices[j] = new Speach();
+				if (replice.StartsWith("#"))
+				{
+					Debug.Log($"Replica: {replice}");
+					var d2 = replice.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+					replices[i / 2].DayStartReplices[j].AnimPos = d2[0].Substring(1);
+
+					replice = d2[1];
+				}
+
+				replices[i / 2].DayStartReplices[j].Replices = replice;
+			}
+			for (int j = 0; j < dialogues[i+1].Length; j++)
+			{
+				replices[i / 2].FoodEndReplices[j] = new Speach();
+				replices[i / 2].FoodEndReplices[j].Replices += dialogues[i + 1][j];
+			}
+		}
+
+		this.replices = replices;
+	}
+
+	public List<string[]> Parse1()
+	{
+		string text = "#DemonAppear\r\nВаа!!!\r\n\r\n#DemonDefaultPose\r\nДоброе утро, Лошара! Ой, я ошибся, я имел ввиду... Ночь! Да!\r\n\r\n#DefaultFaceSpeech1\r\nЧего хмурый такой, не узнаешь это место? Это же твой дом, Балбес!\r\n\r\n#DefaultFaceSpeech1\r\nНу и дерьмо. Хватит считать овец на небе и приготовь мне что ни будь.\r\n\r\n#DefaultFaceSpeech3\r\nИ чего стоишь? Думаешь можешь мне перечить? \r\n\r\n#DefaultFaceSpeech2\r\nХах, я бы посмотрел на твою несгибаемость когда ты увидишь свою семью.\r\n\r\n#ShowFamily \r\nДа-да, твоя семья тут! Все семеро, как те козлята. А я получается волк.\r\n\r\n#JoyPose2\r\nО, уже рвешься и мечешь? Рад слышать, тогда ты знаешь что делать, я проголодался.\r\n\r\n#DefaultFaceSpeech1\r\n... Ты что, никогда в жизни не готовил?\r\n\r\n#DefaultFaceSpeech1\r\nВот же ж бестолочь, придется тебя всему обучать.\r\n\r\n#ShowSpirits\r\nИ так, слушай сюда, Лошара, справа от тебя находится полка, где покоятся твои предки.\r\n\r\n#ShowBoiler \r\nКаждый из них дает тебе силу, уникальную. Выбери ту которую не жалко и закидывай в котел.\r\n\r\n#SmilePose2\r\nИ побыстрее! Я голоден и готов съесть семерых твоих семьи, если ты не поторопишься!\r\n\r\n\r\n#JoyPose1\r\nУуухх, вышло очень даже аппетитно! Из тебя хороший повар, А?!\r\n\r\n#JoyPose1\r\nИ чего ты так расстроился? Че, предков жалко стало? \r\n\r\n#DefaultFaceSpeech2 \r\nДа забей ты на них. Они свое уже прожили, и пускай уже окончательно сгинут! А тебе надо свою шкуру спасать!\r\n\r\n#SmilePose1\r\nНу... Свою семью, да.\r\n\r\n#ShowCandle\r\nЛадно, проехали. Видишь ту свечку? Сдуй ка одну из его пламеней.\r\n\r\n#SmilePose1\r\nИнтересно стало что за свеча? \r\n\r\n#JoyPose1\r\nНу, такому тупому как ты, сложно будет дотянуться до моих гениальных мыслей, но это моя личная разработка. \r\n\r\n#JoyPose2\r\nКаждый день надо сдувать по одной и когда все пламени угаснут, это будет означать конец новолунию. А значит и конец моей свободе...\r\n\r\n#DefaultFaceSpeech3\r\n... Аж на слезу пробило... Отставить!\r\n\r\n#ShowCandle\r\nПотуши ты эту гребанную свечку и пойдем уже в следующий день!\r\n\r\n\r\n#DefaultFaceSpeech2 \r\nДоброй ночи, Лошок!\r\n\r\n#DefaultFaceSpeech3\r\nКак дела? Как ощущения? Отлично себя чувствуешь после вчерашнего? А то!\r\n\r\nТак, слушай сюда, сегодня у меня трудный денек выдался и я кушать хочу. Приготовь ка мне одного из своих деликатесов!\r\n\r\n#DefaultFaceSpeech1\r\nИ чтобы без этого твоего упрямства! Понял, Лошара?!\r\n\r\n\r\n#SmilePose2\r\nОоо какая вкуснотень! Это без преувеличений достойно пяти звезд Мишлена!\r\n\r\n#JoyPose2\r\nИз тебя потомок такой себе, не думал стать повором у меня? Ахахахаах\r\n\r\n#SmilePose1\r\nХотя нет, ведь так ты и меня в котел запихаешь, ведь у тебя к этому какая та личная тяга! Разве нет?!\r\n\r\nНу и ладно, не смешно уже стало. Какой же ты жалкий! Пропусти уже день.";
+
+		Debug.Log(text);
+
+		string[] dialogueBlocks =
+			text.Split("\r\n\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+
+		List<string[]> dialogues = new();
+
+		foreach (string block in dialogueBlocks)
+		{
+			string[] lines =
+				block.Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+			dialogues.Add(lines);
+		}
+
+		return dialogues;
+	}
+
+
 }
